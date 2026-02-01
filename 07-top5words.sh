75756239663a6d028d550331f0a6714ee14b5075
Prasanth Reddy <prasanthreddy1792@gmail.com>
2026-02-01 12:20:32 +0530

    shell practice

---
+++ 07-top5words.sh
@@ -0,0 +1,17 @@
+#!/bin/bash
+
+FILE=$1
+
+if [[ ! -f $FILE ]]; then
+    echo " file $FILE not found "
+    exit 1
+fi
+
+cat "$FILE" | \
+    tr -c '[:alnum:]' '[\n*]' | \
+    tr '[:upper:]' '[:lower:]' | \
+    grep -v '^$' | \
+    sort | \
+    uniq -c | \
+    sort -nr | \
+    head -5
