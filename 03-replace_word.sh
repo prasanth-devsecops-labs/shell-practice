21a10887dddb12b3caf02008599a1decce56f36c
Prasanth Reddy <prasanthreddy1792@gmail.com>
2026-02-01 09:44:57 +0530

    shell practice

--- 03-replace_word.sh
+++ 03-replace_word.sh
@@ -5,3 +5,8 @@ SEACRHDIR="${1:-.}"
 find "$SEACRHDIR" -type f -name "*.conf" -exec sed 's/sbin/SBIN/g' {} +
 
 echo "success...."
+
+
+for f in $(find $SEACRHDIR -type f -name "passwd"); do
+    sed 's/sbin/SBIN/g' $f | diff -u "$f" -
+done
