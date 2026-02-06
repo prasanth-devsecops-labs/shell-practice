ece53133aa549d8dd0acacd2a709cb7ddba050fa
Prasanth Reddy <prasanthreddy1792@gmail.com>
2026-02-06 12:37:15 +0530

    shell practice

--- mail.sh
+++ mail.sh
@@ -7,7 +7,8 @@ TO_TEAM=$6
 
 FINAL_MESSAGE_BODY=$(echo $MESSAGE_BODY | sed -e 's/[]\/$*.^[]/\\&/g')
 
-FINAL_MESSAGE=$(sed -e "s/TO_TEAM/$6/g" -e "s/ALERT_TYPE/$4/g" -e "s/SERVER_IP/$5/g" -e "s/MESSAGE/$3/g" template.html)
+FINAL_MESSAGE=$(sed -e "s|TO_TEAM|$6|g" -e "s|ALERT_TYPE|$4|g" -e "s|SERVER_IP|$5|g" -e "s|MESSAGE|$3|g" template.html)
+
 
 {
 echo "To: $TO_ADDRESS"
