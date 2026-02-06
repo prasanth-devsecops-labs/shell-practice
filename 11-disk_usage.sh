7c890e5070f4901db77b4e37a26c5f12b334044e
Prasanth Reddy <prasanthreddy1792@gmail.com>
2026-02-06 12:34:39 +0530

    shell practice

--- 11-disk_usage.sh
+++ 11-disk_usage.sh
@@ -16,7 +16,9 @@ SCRIPT_NAME=$(basename "$0")
 LOG_FILE="${LOGS_FOLDER}/backup.log"
 mkdir -p "$LOGS_FOLDER"
 
+MESSAGE=""
 THRESHOLD=4
+IP_ADDRESS=$(curl http://169.254.169.254/latest/meta-data/local-ipv4)
 
 log() {
     echo -e "$(date "+%Y-%m-%d-%H:%M:%S") $1" | tee -a $LOG_FILE
@@ -28,7 +30,8 @@ while IFS= read line; do
     USAGE=$(echo $line | awk '{print $6}' | cut -d "%" -f1)
     PARTITION=$(echo $line | awk '{print $7}')
     if [ "$USAGE" -gt "$THRESHOLD" ]; then
-        log "High disk usage on : $PARTITION:$USAGE%"
+        MESSAGE+="High disk usage on : $PARTITION:$USAGE%"
     fi
 done <<< $DISK_USAGE
 
+sh mail.sh "prasanthreddy1792@gmail.com" "High disk usage alert on $IP_ADDRESS" "$MESSAGE" "HIGH_DISK_USAGE" "$IP_ADDRESS" "DEVOPS Team"
---
+++ mail.sh
@@ -0,0 +1,18 @@
+TO_ADDRESS=$1
+SUBJECT=$2
+MESSAGE_BODY=$3
+ALERT_TYPE=$4
+SERVER_IP=$5
+TO_TEAM=$6
+
+FINAL_MESSAGE_BODY=$(echo $MESSAGE_BODY | sed -e 's/[]\/$*.^[]/\\&/g')
+
+FINAL_MESSAGE=$(sed -e "s/TO_TEAM/$6/g" -e "s/ALERT_TYPE/$4/g" -e "s/SERVER_IP/$5/g" -e "s/MESSAGE/$3/g" template.html)
+
+{
+echo "To: $TO_ADDRESS"
+echo "Subject: $SUBJECT"
+echo "Content-Type: text/html"
+echo ""
+echo "$FINAL_MESSAGE"
+} | msmtp "$TO_ADDRESS"
